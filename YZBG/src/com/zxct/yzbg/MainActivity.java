5844c9aae73131a55ed05023eac0d08a8faa2f7d
Bao <Bao@Bao>
2014-09-20 12:31:16 +0800

    第一次提交

---
+++ YZBG/src/com/zxct/yzbg/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zxct.yzbg;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		 //super.loadUrl("file:///android_asset/www/index.html");
+		super.loadUrl("file:///android_asset/www/workflow/SPIndex.html");	
+		
+	}
+
+}
